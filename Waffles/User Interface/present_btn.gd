9ecdaf242c3b66ec52d7599b2966894e948d1a00
Lukas Marusenko <63213730+Lukas311202@users.noreply.github.com>
2023-01-11 20:15:19 +0100

    upload updated files

--- Waffles/User Interface/present_btn.gd
+++
@@ -1,6 +0,0 @@
-extends TextureButton
-
-
-
-func _on_present_pressed():
-	get_parent().get_node("player_inventory").select_item_from_inventory()
--- Waffles/global_scripts/DialugueConditions.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func check_condition(condition) ->bool:
-	if condition == "mystery":
-		return false
-	if condition == "friends":
-		return GlobalDialogue.current_speaker.pl_relation >= 50
-	
-	return true
